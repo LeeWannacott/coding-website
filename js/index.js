b50a2b8419939a428baab2ae6729506df9543025
LeeWannacott <WannacottL@gmail.com>
2023-04-11 23:01:41 +1200

    remove index.js for now.

--- js/index.js
+++
@@ -1,5 +0,0 @@
-import * as monaco from "./node_modules/monaco-editor/esm/vs/editor/edcore.main/index.js";
-monaco.editor.create(document.getElementById("monaco-editor"), {
-  value: ["function x() {", '\tconsole.log("Hello world!");', "}"].join("\n"),
-  language: "javascript",
-});
