8d2f7e6f1225aedbc17c238988fbe08a7c1f8abc
LeeWannacott <WannacottL@gmail.com>
2023-04-11 21:50:25 +1200

    WIP: 2. trying to get monaco working.

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func codeChallenge(w http.ResponseWriter, request *http.Request) {
 		fmt.Println(code)
 		// fmt.Fprintf(w, "Name = %s\n", code)
 		// fmt.Println("Code: ", request)
-
+		w.Header().Set("Content-Type", "text/plain")
 		http.ServeFile(w, request, "index.html")
 		// http.PostForm("http://localhost:8080", request.Form)
 	default:
@@ -43,8 +43,10 @@ func codeChallenge(w http.ResponseWriter, request *http.Request) {
 
 func main() {
 
-	fs := http.FileServer(http.Dir("css"))
-	http.Handle("/css/", http.StripPrefix("/css/", fs))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	http.Handle("/tmp/", http.StripPrefix("/tmp/", http.FileServer(http.Dir("tmp"))))
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js"))))
+	http.Handle("/node_modules/", http.StripPrefix("/node_modules/", http.FileServer(http.Dir("node_modules"))))
 	http.HandleFunc("/", codeChallenge)
 
 	fmt.Printf("Starting server for testing HTTP POST...\n")
